25389f5ea71d254ed50cc1f03e139af2a51614af
Morten Moesgaard <morten@moesgaards.dk>
2014-07-30 10:05:54 +0200

    we remove  style.css as it  will have no use at all

--- db/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->handle['pdo_mysql_User'][0] = '';
-$this->handle['pdo_mysql_Host'][0] = '';
-$this->handle['pdo_mysql_Password'][0] = '';
-$this->handle['pdo_mysql_Database'][0] = '';
-$this->handle['Primary'][0] = 'pdo_mysql';
-?>
